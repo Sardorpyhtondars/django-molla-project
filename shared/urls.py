7da6b0b565950a680adfdfddc7d0ce6939356597
Sardorpythondars <sardoristamov1212@gmail.com>
2026-03-09 16:07:54 +0500

    models added

--- shared/urls.py
+++ shared/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from shared import views
+from shared.views import home_page_view, about_us_view, contact_view, page_404_view
 
 app_name = 'shared'
 
 urlpatterns = [
-    path('', views.home_page_view, name='home'),
-    path('about/', views.about_us_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),
+    path('', home_page_view, name='home'),
+    path('about/', about_us_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('404/', page_404_view)
 ]
